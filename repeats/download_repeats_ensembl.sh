4408405eab1d24732372a637a584538ef09a9d62
brunocontrerasmoreira <bcontreras@ebi.ac.uk>
2020-10-23 18:01:06 +0100

    up to task 10

--- repeats/download_repeats_ensembl.sh
+++ repeats/download_repeats_ensembl.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # Script to download the annotated repeated elements of a selected
-# Ensembl Genomes species
+# Ensembl Plants species. 
+
+# NOTE: required binaries: wget, bedtools, sort, perl, mysql, cd-hit-est
 
 # Copyright [2020] EMBL-European Bioinformatics Institute
 
@@ -9,21 +11,25 @@
 # http://www.ensembl.org/info/docs/api/index.html
 
 if [[ $# -eq 0 ]] ; then
-	echo "# usage: $0 species_name, ie arabidopsis_thaliana"
+	echo "# example usage: $0 arabidopsis_thaliana"
 	exit 0
 else
 	SPECIES=$1
 fi
 
-# set server details
+# PARAMS
+MINLEN=90
+MAXDEGENPERC=10
+
+# SERVER DETAILS
+FTPSERVER="ftp://ftp.ensemblgenomes.org/pub"
+DIV=plants
 SERVER=mysql-eg-publicsql.ebi.ac.uk
 USER=anonymous
 PORT=4157
 
-# get Ensembl Plants current release number from FTP server
+## 1) get Ensembl Plants current release number from FTP server
 # Note: wget is used, this can be modified to use alternatives ie curl
-FTPSERVER="ftp://ftp.ensemblgenomes.org/pub"
-DIV=plants
 SUMFILE="${FTPSERVER}/${DIV}/current/summary.txt"
 RELEASE=`wget --quiet -O - $SUMFILE | \
 	perl -lne 'if(/Release (\d+) of Ensembl/){ print $1 }'`
@@ -31,26 +37,61 @@ RELEASE=`wget --quiet -O - $SUMFILE | \
 # work out Ensembl Genomes release
 EGRELEASE=$(( RELEASE - 53));
 
+## 2) select core db matching selected species
 SPECIESCORE=$(mysql --host $SERVER --user $USER --port $PORT \
 	-e "show databases" | grep "${SPECIES}_core_${EGRELEASE}_${RELEASE}")
 
-MINLEN=90
+## 3) retrieve 1-based coords of repeats
 
-# note these might be reduntant
+# note these might be redundant/overlapping
 #1       3       106     trf
 #1       4       91      trf
-#1       755     761     dust
-#1       1064    1098    trf
-#1       1066    1097    trf
 
-mysql --host $SERVER --user $USER --port $PORT $SPECIESCORE -Nb -e "SELECT sr.name,r.seq_region_start,r.seq_region_end,rc.repeat_class FROM repeat_feature r JOIN seq_region sr JOIN repeat_consensus rc WHERE r.seq_region_id=sr.seq_region_id AND r.repeat_consensus_id=rc.repeat_consensus_id AND (r.seq_region_end-r.seq_region_start+1) > $MINLEN  ORDER BY sr.name,r.seq_region_start" > ${SPECIES}.repeats.bed
+mysql --host $SERVER --user $USER --port $PORT $SPECIESCORE -Nb -e \
+	"SELECT sr.name,r.seq_region_start,r.seq_region_end,rc.repeat_class \
+	FROM repeat_feature r JOIN seq_region sr JOIN repeat_consensus rc \
+	WHERE r.seq_region_id=sr.seq_region_id \
+	AND r.repeat_consensus_id=rc.repeat_consensus_id \
+	AND (r.seq_region_end-r.seq_region_start+1) > $MINLEN \
+	ORDER BY sr.name,r.seq_region_start" > _${SPECIES}.repeats.bed
 
-mysql --host $SERVER --user $USER --port $PORT $SPECIESCORE -Nb -e "SELECT sr.name,g.seq_region_start,g.seq_region_end,g.stable_id FROM gene g JOIN seq_region sr WHERE g.seq_region_id=sr.seq_region_id  ORDER BY sr.name,g.seq_region_start" > ${SPECIES}.genes.bed
+## 4) retrieve 1-based coords of genes 
+mysql --host $SERVER --user $USER --port $PORT $SPECIESCORE -Nb -e \
+	"SELECT sr.name,g.seq_region_start,g.seq_region_end,g.stable_id \
+	FROM gene g JOIN seq_region sr \
+	WHERE g.seq_region_id=sr.seq_region_id \
+	ORDER BY sr.name,g.seq_region_start" > _${SPECIES}.genes.bed
 
-~/soft/bedtools2/bin/bedtools subtract -a ${SPECIES}.repeats.bed -b ${SPECIES}.genes.bed > ${SPECIES}.repeats.curated.bed
+## 5) curate repeats by substracting annotated genes
+bedtools subtract -a _${SPECIES}.repeats.bed -b _${SPECIES}.genes.bed >\
+	_${SPECIES}.repeats.curated.bed
 
-sort -k1,1 -k2,2n ${SPECIES}.repeats.curated.bed > ${SPECIES}.repeats.sorted.bed
 
-~/soft/bedtools2/bin/bedtools merge -c 4 -o collapse -i ${SPECIES}.repeats.sorted.bed > ${SPECIES}.repeats.merged.bed
+## 6) sort and convert to 0-based BED
+sort -k1,1 -k2,2n _${SPECIES}.repeats.curated.bed | \
+	perl -lane '$F[1]-=1; print join("\t",@F)' >\
+	_${SPECIES}.repeats.sorted.bed
 
-mysql --host $SERVER --user $USER --port $PORT $SPECIESCORE -Nb -e "SELECT sr.name,d.sequence FROM dna d JOIN seq_region sr WHERE d.seq_region_id=sr.seq_region_id  ORDER BY sr.name" | perl -lane 'print ">$F[0]\n$F[1]\n"' > ${SPECIES}.repeats.dna
+## 7) download and uncompress genomic sequence 
+FASTA="*${SPECIES^}*.dna.toplevel.fa.gz"
+URL="${FTPSERVER}/${DIV}/current/fasta/${SPECIES}/dna/${FASTA}"
+echo "# downloading $URL"
+wget -c $URL -O- | gunzip > _${SPECIES}.dna
+
+## 8) extract repeat sequences 
+bedtools getfasta -name -fi _${SPECIES}.dna -bed _${SPECIES}.repeats.sorted.bed >\
+	_${SPECIES}.repeats.dna
+
+## 9) eliminate degenerate (MAXDEGENPERC) 
+cat _${SPECIES}.repeats.dna | \
+	perl -slne 'if(/^(>.*)/){$h=$1} else {$fa{$h}.=$_} END{ foreach $h (keys(%fa)){ $l=length($fa{$h}); $dg=($fa{$h}=~tr/Nn//); print "$h\n$fa{$h}" if(100*$dg/$l<=$maxdeg) }}' \
+	-- -maxdeg=$MAXDEGENPERC > _${SPECIES}.repeats.nondeg.dna
+
+# 10) eliminate short and redundant sequences 
+cd-hit-est -c 0.99 -l $MINLEN -i _${SPECIES}.repeats.nondeg.dna \
+	-o ${SPECIES}.${EGRELEASE}.repeats.nr.dna
+
+# 11) clean temp file
+
+
+exit 0
